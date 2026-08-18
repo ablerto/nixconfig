8afa0c235ec25c6cc963de0a91fbbb934444fb9b
ablerto <albertroed@hotmail.com>
2026-08-18 12:11:50 +0200

    add home manager

--- hosts/common/users/alberthr.nix
+++ hosts/common/users/alberthr.nix
@@ -1,10 +1,9 @@
 {
-    config,
-    pkgs,
-    inputs,
-    ...
+  config,
+  pkgs,
+  inputs,
+  ... 
 } : {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.alberthr = {
     isNormalUser = true;
     initialHashedPassword = "$y$j9T$i2Oh9OeA/I2Pttz/Ox0jB1$vdMpBLdrleV2sQeqZ0BJXQBwgsNxID3vj3yM8JJ2CA5";
@@ -21,9 +20,7 @@
       "kvm"
       "qemu-libvirtd"
     ];
-    packages = [
-      inputs.nome-manager.packages.${pkgs.system}.default
-    ];
-    home-manager.users.alberthr = import alberthr/${config.networking.hostName}.nix;
+    packages = [inputs.home-manager.packages.${pkgs.system}.default];
   };
+  home-manager.users.alberthr = import alberthr/${config.networking.hostName}.nix;
 }
